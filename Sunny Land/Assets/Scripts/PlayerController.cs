adf5f42f3b736a369a39740bf30a9123f4f038cd
RZCen <100001718+RZCen@users.noreply.github.com>
2022-05-09 13:29:11 -0400

    camera

--- Sunny Land/Assets/Scripts/PlayerController.cs
+++ Sunny Land/Assets/Scripts/PlayerController.cs
@@ -4,39 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public Collider2D coll;
     public float speed;
     public float jumpforce;
+    public LayerMask ground;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        SwitchAnim();
     }
 
     void Movement()
     {
         float horizontalmove = Input.GetAxis("Horizontal");
         float facedircetion = Input.GetAxisRaw("Horizontal");
-
+        //move
         if(horizontalmove != 0)
         {
             rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedircetion)); ;
         }
         if(facedircetion != 0)
         {
             transform.localScale = new Vector3(facedircetion, 1, 1);
         }
-
+        //jump
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            anim.SetBool("jumping", true);
+        }
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetBool("idle", false);
+        if (anim.GetBool("jumping"))
+        {
+            if(rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idle", true);
         }
     }
 }
