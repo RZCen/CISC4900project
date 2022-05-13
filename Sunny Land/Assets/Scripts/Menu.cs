28520dfe04239068f2f1b2742101d0622bab82bf
RZCen <100001718+RZCen@users.noreply.github.com>
2022-05-13 13:57:39 -0400

    pause menu

--- Sunny Land/Assets/Scripts/Menu.cs
+++ Sunny Land/Assets/Scripts/Menu.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +17,27 @@ public class Menu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("MainVolume", value);
     }
 }
